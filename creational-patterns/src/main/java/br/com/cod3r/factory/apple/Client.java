8eb56e46f4c3b72e825f98de58517407b0cfeb99
Miria A S Santos <73804490+MiiSantos@users.noreply.github.com>
2025-01-24 18:52:39 +0000

    factory pratice

--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,11 +1,18 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.model.IPhone;
+
 public class Client {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("### Ordering an iPhone X");
+		IPhone iphone = IPhoneFactory.orderIphone("X", "standart");
+		System.out.println(iphone);
 		
-		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+		System.out.println("\n\n### Ordering an iPhone 11");
+		IPhone iphone2 = IPhoneFactory.orderIphone("11", "standart");
+		System.out.println(iphone2);
 	}
 }
--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -1,6 +1,44 @@
 package br.com.cod3r.factory.apple.factory;
 
+import br.com.cod3r.factory.apple.model.IPhone;
+import br.com.cod3r.factory.apple.model.IPhone11;
+import br.com.cod3r.factory.apple.model.IPhone11Pro;
+import br.com.cod3r.factory.apple.model.IPhoneX;
+import br.com.cod3r.factory.apple.model.IPhoneXSMax;
+
 public abstract class IPhoneFactory {
 
-	
+    public static final String IPHONE_X = "X";
+    public static final String IPHONE_11 = "11";
+    public static final String STANDART = "standart";
+    public static final String HIGH_END = "highEnd";
+
+    public static IPhone orderIphone(String gen, String level) {
+        IPhone device = null;
+
+        if(IPHONE_X.equals(gen)) {
+            if(STANDART.equals(level)) {
+                device = new IPhoneX();
+            } else if (HIGH_END.equals(level)) {
+                device = new IPhoneXSMax();
+            }
+        } else if (IPHONE_11.equals(gen)) {
+            if(STANDART.equals(level)) {
+                device = new IPhone11();
+            } else if (HIGH_END.equals(level)) {
+                device = new IPhone11Pro();
+            }
+        }
+
+        if(device != null) {
+			device.getHardware();
+			device.assemble();
+			device.certificates();
+			device.pack();
+
+			return device;
+		}
+
+        return null;
+    }
 }
